e85269999341070e2f884fa3f6a815e414a5d9d1
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-12 18:13:17 +0530

    commit till 1.09 hrs
    commit till 1.09 hrs

--- my-first-react19-project/src/App.jsx
+++ my-first-react19-project/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Search from "./components/search";
+import { useState, useEffect } from "react";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
 }
 
-export default App
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async() => {
+    try {
+      
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+    }
+  }
+
+  useEffect(effect:() => {
+
+  }, [])
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without The Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
